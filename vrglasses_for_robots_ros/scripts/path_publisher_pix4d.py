a58c8ee625f1601e49aa477e0960a20bbf7de84d
Lucas Teixeira <weblucas@gmail.com>
2021-09-05 06:30:55 +0200

    pix4d working with hack to split the dataset

--- vrglasses_for_robots_ros/scripts/path_publisher_pix4d.py
+++ vrglasses_for_robots_ros/scripts/path_publisher_pix4d.py
@@ -97,74 +97,6 @@ def load_pix4d():
 
     return points, orientations, filenames
 
-def single_sample(num, x=0, y=0, height=100, yaw=90):
-    points = []
-    yaws = []
-
-    for i in range(num):
-        points.append((x, y, height))
-        yaws.append(yaw)
-    return points, yaws
-
-def star_positions_old(center_x=0.0, center_y=0.0, min_radius=10, max_radius=120, height=50.0):
-    points = []
-    yaw = []
-
-    radius_step = 10;
-
-    min_angle = 0;
-    max_angle = 360;
-    angle_step = 5;
-
-    for r in range(min_radius, max_radius, radius_step):
-        for a in range(min_angle, max_angle, angle_step):
-            angle_rad = np.deg2rad(a);
-            x = center_x + (r * np.cos(angle_rad))
-            y = center_y + (r * np.sin(angle_rad))
-            points.append((x, y, height))
-            yaw.append(a + 180)  # a+ 180
-
-    return points, yaw
-
-
-def star_positions(center_x=0.0, center_y=0.0, min_radius=10, max_radius=221, height=50.0, radius_step=40):
-    points = []
-    yaw = []
-
-    radius_step = radius_step
-
-    min_angle = 0;
-    max_angle = 360;
-    angle_step = 5;
-    print('Height: {}'.format(height))
-    for a in range(min_angle, max_angle, angle_step):
-        angle_rad = np.deg2rad(a);
-        yaw_cam = - a
-        for r in range(min_radius, max_radius, radius_step):
-            x = center_x + (r * np.cos(angle_rad))
-            y = center_y + (r * np.sin(angle_rad))
-            # print("({},{},{})".format(x,y,yaw_cam))
-            points.append((x, y, height))
-            yaw.append(yaw_cam)
-
-    return points, yaw
-
-
-def compute_star_orientations(points, yaw_angles, pitch):
-    orientations = []
-    print('Pitch Angle: {}'.format(pitch))
-    roll = 0
-    nPoints = len(points)
-    for i in range(nPoints):
-        r_rad = np.deg2rad(roll);
-        p_rad = np.deg2rad(pitch);  # descent angle
-        y_rad = np.deg2rad(yaw_angles[i]);  # direction of arrival (star line)
-        # print(yaw_angles[i])
-        quat = quaternion_from_euler(y_rad, p_rad, r_rad)
-        orientations.append((quat[0], quat[1], quat[2], quat[3]))  # w, x, y, z
-
-    return orientations
-
 
 def publisher( points, orientations,filenames, rate=100):
     pub = rospy.Publisher('firefly/vi_sensor/ground_truth/odometry', Odometry, queue_size=1)  # pose_publisher/odometry
@@ -187,8 +119,8 @@ def publisher( points, orientations,filenames, rate=100):
 
     file_dir = '/home/lucas/Downloads/random_poses.txt'
     with open(file_dir, 'w') as f:
-        print('Number of Points: {}'.format(len(points)))
-        for i in tqdm(range(0, 270)):#len(points))):
+        print('Number of Points: {}'.format(len(points)-270))
+        for i in tqdm(range(270, len(points))):
             p = Odometry()
             p.header.stamp = rospy.Time.now()
             p.header.frame_id = 'world'
@@ -223,41 +155,6 @@ def publisher( points, orientations,filenames, rate=100):
             rate.sleep()
 
     f.close()
-
-
-def plot_position(points_array, yaws_array, x_min=-95, x_max=175, y_min=-120, y_max=120, split_factor=0.8):
-    fig = plt.figure(figsize=(20, 12))
-    ax = plt.subplot()
-    X_train = []
-    Y_train = []
-    angle_train = []
-    X_val = []
-    Y_val = []
-    angle_val = []
-
-    for index in range(int(split_factor * len(points))):
-        X_train.append(points[index][0])
-        Y_train.append(points[index][1])
-        angle_rad_train = np.deg2rad(180 - yaws_array[index])
-        u = math.sin(angle_rad_train)
-        v = math.cos(angle_rad_train)
-        ax.quiver(points[index][0], points[index][1], v, u, color='b', width=0.001, scale=80)
-    for index in range(int(split_factor * len(points)), len(points)):
-        X_val.append(points[index][0])
-        Y_val.append(points[index][1])
-        angle_rad_val = np.deg2rad(180 - yaws_array[index])
-        u = math.sin(angle_rad_val)
-        v = math.cos(angle_rad_val)
-        ax.quiver(points[index][0], points[index][1], v, u, color='r', width=0.001, scale=80)
-
-    ax.scatter(X_train, Y_train, alpha=1, color='blue', label='Training Set')
-    ax.scatter(X_val, Y_val, alpha=1, color='red', label='Validation Set')
-    ax.set_title('Sampling Position Visualization')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend(loc=1)
-    fig.savefig('/media/shiming/Ming_Passport/MA_ws/pose_publisher_plots/sampling_position_1.png')
-    print("Plot is saved")
 
 
 if __name__ == '__main__':
