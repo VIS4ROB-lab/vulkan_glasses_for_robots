c67459de2510948529787a3af4a51903d2e5874f
fmaffra <fabiola.maffra@gmail.com>
2021-10-18 14:33:31 +0200

    aspect ratio issue solved, just need to fix the imu pose issue

--- vrglasses_for_robots_ros/scripts/ortho_model_top_view.py
+++ vrglasses_for_robots_ros/scripts/ortho_model_top_view.py
@@ -13,13 +13,13 @@ def model_limits():
         material.vertices
 
         # extract all x coordinates (every 8th element starting with 6th)
-        xVertices = material.vertices[2::5]
+        xVertices = material.vertices[5::8]
 
         # extract all y coordinates (every 8th element starting with 7th)
-        yVertices = material.vertices[3::5]
+        yVertices = material.vertices[6::8]
 
         # extract all z coordinates (every 8th element sarting with 8th)
-        zVertices = material.vertices[4::5]
+        zVertices = material.vertices[7::8]
 
         # calculate min/max of coordinates
         x_max = max(xVertices)
@@ -40,16 +40,16 @@ def model_limits():
         diff_y = y_max - y_min
         diff_z = z_max - z_min
         diff_max = max(diff_x,diff_y)
-        half_diff_max = diff_max / 2.0
+        half_diff_max = diff_max / 2.0 # assumes a square output image
 
-        x_cam = x_min + ((x_max - x_min) / 2.0)
-        y_cam = y_min + ((y_max - y_min) / 2.0)
+        x_cam = x_min + (diff_x / 2.0)
+        y_cam = y_min + (diff_y / 2.0)
         z_cam = z_max+ 2.0   # z needs to be bigger than z_max
-        l = - diff_x/ 2.0
-        r = diff_x/ 2.0
+        l = - half_diff_max + y_cam
+        r = half_diff_max + y_cam
 
-        b = - diff_y/ 2.0
-        t = diff_y/ 2.0
+        b = - half_diff_max + x_cam
+        t = half_diff_max + x_cam
 
         print(l, r, b, t, x_cam, y_cam, z_cam)
         return l, r, b, t, x_cam, y_cam, z_cam
