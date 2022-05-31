49dac764436da5718d0e70b22dbbb6f31ce743f9
lucaBartolomei <lbartolomei@ethz.ch>
2022-05-31 16:33:10 +0200

    Add multi camera support (you can specify as many cameras as you need and their transformation w.r.t. body frame)

--- vrglasses_for_robots_ros/include/vrglasses_for_robots_ros/vrglasses_node.h
+++ vrglasses_for_robots_ros/include/vrglasses_for_robots_ros/vrglasses_node.h
@@ -11,6 +11,7 @@
 #include <eigen3/Eigen/Core>
 
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <image_transport/image_transport.h>
 #include <kindr/minimal/quat-transformation.h>
@@ -19,7 +20,6 @@
 namespace  vrglasses_for_robots{
 class VulkanRenderer;
 }
-
 
 class VRGlassesNode {
 public:
@@ -31,91 +31,82 @@ public:
     bool initialized() const { return initialized_; }
 
 private:
-
-    //parameters
-    ros::Duration diff_frames_;
-    ros::Time last_frame_time_;
-
-    bool stereo_;
-    bool initialized_;
-
     // ros
     ::ros::NodeHandle nh_;
     ::ros::NodeHandle nh_private_;
-    ::ros::Subscriber odom_sub_; //usually imu (sensor S) pose of visim (T_WS)
-    ::ros::Publisher  camera_odom_pub_; // camera in the model world frame (T_WC)
-
-    image_transport::CameraPublisher cam_l_pub_;    // primary publisher
-    image_transport::CameraPublisher cam_r_pub_;    // only used for stereo
-
-    //image_transport::ImageTransport rgb_imt_;
-    ros::Publisher dense_pointcloud_pub_;
-
-    image_transport::Publisher semantic_pub_;
-    //image_transport::ImageTransport semantic_imt_;
-
-    image_transport::Publisher depth_pub_;
-    //image_transport::ImageTransport depth_imt_;
     image_transport::ImageTransport image_transport_;
 
-    sensor_msgs::CameraInfo cam_info_l_;
-    sensor_msgs::CameraInfo cam_info_r_;
+    ::ros::Subscriber odom_sub_; //usually imu (sensor S) pose of visim (T_WS)
+    std::vector<::ros::Publisher> cameras_odom_pub_; // camera in the model world frame (T_WC)
+
+    std::vector<image_transport::CameraPublisher> cams_pub_;
+    std::vector<image_transport::Publisher> semantic_pub_;
+    std::vector<image_transport::Publisher> depth_pub_;
+    std::vector<sensor_msgs::CameraInfo> cams_info_;
+
+    // Add TF publisher to have a transformation tree that makes sense
+    std::vector<tf::TransformBroadcaster> tf_broadcasters_;
+    std::vector<tf::StampedTransform> tf_cameras_;
+
 
     glm::mat4 computeMVP(const kindr::minimal::QuatTransformation &pose);
 
-    kindr::minimal::QuatTransformation computeT_WC_left(const geometry_msgs::Pose &pose);
-    kindr::minimal::QuatTransformation computeT_WC_right(const geometry_msgs::Pose &pose);
+    kindr::minimal::QuatTransformation computeT_WC_camera(size_t cam_id, const geometry_msgs::Pose &pose) const;
 
     void odomCallback(const nav_msgs::Odometry &msg);
-
-    void publishDenseSemanticCloud(const std_msgs::Header header, const sensor_msgs::ImagePtr &depth_map, const cv::Mat& semantic_map);
 
     // rendering
     vrglasses_for_robots::VulkanRenderer* renderer_;
     glm::mat4 perpective_;
     double near_ = 0.1, far_ = 500.0;
 
-    cv::Mat result_depth_map_l_, result_rgbs_map_l_,result_rgb_map_l_,result_s_map_l_;
-    cv::Mat result_depth_map_r_, result_rgbs_map_r_,result_rgb_map_r_,result_s_map_r_;
+    std::vector<cv::Mat> result_depth_map_, result_rgbs_map_, result_rgb_map_, result_s_map_;
 
     void buildOpenglProjectionFromIntrinsics(
             glm::mat4& matPerspective, glm::mat4& matProjection,
             /*glm::mat4& matCVProjection,*/ int img_width, int img_height, float alpha,
             float beta, float skew, float u0, float v0, float near, float far);
 
+    // This contains the information for each spawned camera
+    struct Camera {
+      double f;
+      float cx, cy;
+      int w, h;
+
+      Camera() {
+        f = 455;
+        cx = 376.5;
+        cy = 240.5;
+        w = 752;
+        h = 480;
+      }
+    };
+
     class VisimProject {
     public:
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-        double f;
-        float cx, cy;
-        int w, h, sampling_factor, imu_image_delay;
-        double baseline;
-        kindr::minimal::QuatTransformation T_SC_left;
-        kindr::minimal::QuatTransformation T_SC_right;
-        VisimProject() {
-            f = 455;
-            cx = 376.5;
-            cy = 240.5;
-            w = 752;
-            h = 480;
-            imu_image_delay = 1;  // nanosec
+        Camera camera;
+        int sampling_factor, imu_image_delay;
+        std::vector<kindr::minimal::QuatTransformation> T_SCs;
 
+        VisimProject() {
+            imu_image_delay = 1;  // nanosec
             sampling_factor = 10;
 
-            baseline = 0.11;
-            Eigen::Matrix<double, 4, 4> t_sc_left;
-            t_sc_left << 0, 0, 1, 0.015, -1, 0, 0, 0.5*baseline, 0, -1, 0, 0.0065, 0, 0, 0, 1;
-            T_SC_left = kindr::minimal::QuatTransformation(t_sc_left);
-            Eigen::Matrix<double, 4, 4> t_sc_right;
-            t_sc_right << 0, 0, 1, 0.015, -1, 0, 0, -0.5*baseline, 0, -1, 0, 0.0065, 0, 0, 0, 1;
-            T_SC_right = kindr::minimal::QuatTransformation(t_sc_right);
+            // Eigen::Matrix<double, 4, 4> t_sc;
+            // t_sc << 0, 0, 1, 0.015, -1, 0, 0, 0.055, 0, -1, 0, 0.0065, 0, 0, 0, 1;
+            // T_SC = kindr::minimal::QuatTransformation(t_sc);
         }
         // todo load from json project
     };
 
     VisimProject visim_project_;
 
+    //parameters
+    ros::Duration diff_frames_;
+    ros::Time last_frame_time_;
 
+    bool initialized_;
 };
 
 #endif
--- vrglasses_for_robots_ros/src/vrglasses_node.cc
+++ vrglasses_for_robots_ros/src/vrglasses_node.cc
@@ -1,5 +1,5 @@
+#include "vrglasses_for_robots_ros/vrglasses_node.h"
 
-#include <vrglasses_for_robots_ros/vrglasses_node.h>
 #include <iostream>
 #include <vrglasses_for_robots/vulkan_renderer.h>
 #include <opencv2/highgui.hpp>
@@ -7,10 +7,6 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/PointCloud.h>
 #include <minkindr_conversions/kindr_msg.h>
-
-
-
-
 
 VRGlassesNode::VRGlassesNode(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private): nh_(nh), nh_private_(nh_private),
     image_transport_(nh_private_), initialized_(false) {
@@ -26,82 +22,131 @@ VRGlassesNode::VRGlassesNode(const ros::NodeHandle &nh, const ros::NodeHandle &n
     diff_frames_.fromSec(1.0 / framerate);
     last_frame_time_ = ros::Time(0);
 
-    if(!nh_private_.getParam("stereo", stereo_)) {
-      stereo_ = false;
-      ROS_WARN("stereo parameter not found, using default(false)");
-    }
-
-    if(stereo_){
-      if (!nh_private_.getParam("camera_frame_id_l", cam_info_l_.header.frame_id)) {
-        cam_info_l_.header.frame_id = "world";
-        ROS_WARN("camera_frame_id_l parameter not found, using default('world')");
-      }
-      if (!nh_private_.getParam("camera_frame_id_r", cam_info_r_.header.frame_id)) {
-        cam_info_r_.header.frame_id = "world";
-        ROS_WARN("camera_frame_id_r parameter not found, using default('world')");
-      }
-
-      if(cam_info_l_.header.frame_id == cam_info_r_.header.frame_id){
-        ROS_WARN("Both cameras use the same frame! This is unlikely to be correct.");
-      }
-
-    }else{
-      if (!nh_private_.getParam("camera_frame_id", cam_info_l_.header.frame_id)) {
-        cam_info_l_.header.frame_id = "world";
-        ROS_WARN("camera_frame_id parameter not found, using default('world')");
+    int ncameras;
+    if(!nh_private_.getParam("ncameras", ncameras)) {
+        ROS_WARN("ncameras not specified, using default (mono)");
+    } else {
+      if (ncameras <= 0) {
+        ROS_ERROR("Invalid camera configuration specified! Abort.");
+        return;
       }
     }
 
-    // Publishers
-    depth_pub_ = image_transport_.advertise("depth_map", 1);
-    semantic_pub_ = image_transport_.advertise("semantic_map", 1);
-
-    camera_odom_pub_ = nh_private_.advertise<nav_msgs::Odometry>("camera_odometry_out", 50);
-
-    if(stereo_){
-      cam_l_pub_ = image_transport_.advertiseCamera("stereo/left/image", 1);
-      cam_r_pub_ = image_transport_.advertiseCamera("stereo/right/image", 1);
-    }else{
-      cam_l_pub_ = image_transport_.advertiseCamera("color_map", 1);
+    std::string body_frame;
+    if (!nh_private_.getParam("body_frame", body_frame)) {
+      ROS_WARN("body_frame not specified, using default (body)");
+      body_frame = "body";
     }
 
-    cam_info_l_.width = visim_project_.w;
-    cam_info_l_.height = visim_project_.h;
-    cam_info_l_.distortion_model = "plumb_bob";
-    cam_info_l_.D = {0.0, 0.0, 0.0, 0.0, 0.0};
-    cam_info_l_.K = {visim_project_.f, 0.0, visim_project_.cx,
-                     0.0, visim_project_.f, visim_project_.cy,
-                     0.0, 0.0, 1};
-    cam_info_l_.R = {1.0, 0.0, 0.0,
-                     0.0, 1.0, 0.0,
-                     0.0, 0.0, 1.0};
-    cam_info_l_.P = {visim_project_.f, 0.0, visim_project_.cx, 0.0,
-                     0.0, visim_project_.f, visim_project_.cy, 0.0,
-                     0.0, 0.0, 1, 0.0};
+    for (int i = 0; i < ncameras; ++i) {
+      // Get the extrinisics transformation for the camera
+      Eigen::Matrix<double, 4, 4> T_SC(Eigen::Matrix<double, 4, 4>::Identity());
 
-    if(stereo_){
-      cam_info_r_.width = cam_info_l_.width;
-      cam_info_r_.height = cam_info_l_.height;
-      cam_info_r_.distortion_model = cam_info_l_.distortion_model;
-      cam_info_r_.D = cam_info_l_.D;
-      cam_info_r_.K = cam_info_l_.K;
-      cam_info_r_.R = cam_info_l_.R;
+      std::string ns = "camera_" + std::to_string(i) + "/";
+      double roll = 0.0;
+      double pitch = nh_private_.param(ns + "pitch", 0.0);
+      double yaw = nh_private_.param(ns + "yaw", 0.0);
+      Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+      Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+      Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
 
-      double baseline = (visim_project_.T_SC_left.getPosition() - 
+      Eigen::Matrix<double, 3, 3> T_fixed;
+      T_fixed << 0, 0, 1, -1, 0, 0, 0, -1, 0;
+      T_SC.block<3,3>(0,0) = Eigen::Quaterniond(yawAngle * pitchAngle * rollAngle).toRotationMatrix() * T_fixed;
+
+      double p_x = nh_private_.param(ns + "p_x", 0.015);
+      double p_y = nh_private_.param(ns + "p_y", 0.055);
+      double p_z = nh_private_.param(ns + "p_z", 0.0065);
+      T_SC.block<3,1>(0,3) = Eigen::Vector3d(p_x, p_y, p_z);
+
+      visim_project_.T_SCs.push_back(kindr::minimal::QuatTransformation(T_SC));
+
+      // Push back a new camera
+      cams_info_.push_back(sensor_msgs::CameraInfo());
+
+      // Register i-th camera
+      std::string param_name = ns + "camera_frame_id";
+      if (!nh_private_.getParam(param_name,
+                                cams_info_.back().header.frame_id)) {
+        cams_info_.back().header.frame_id = "world";
+        ROS_WARN("%s parameter not found, using default('world')", param_name.c_str());
+      }
+
+      // Check that the frames make sense
+      bool valid = true;
+      for (size_t j = 0; j < cams_info_.size() - 1; ++j) {
+        valid &= cams_info_[j].header.frame_id != cams_info_.back().header.frame_id;
+        if (!valid) {
+          ROS_WARN("Cameras use the same frame! This is unlikely to be correct.");
+          break;
+        }
+      }
+
+      // Fill camera info
+      auto camera = visim_project_.camera;
+      cams_info_.back().width = camera.w;
+      cams_info_.back().height = camera.h;
+      cams_info_.back().distortion_model = "plumb_bob";
+      cams_info_.back().D = {0.0, 0.0, 0.0, 0.0, 0.0};
+      cams_info_.back().K = {camera.f, 0.0, camera.cx,
+                       0.0, camera.f, camera.cy,
+                       0.0, 0.0, 1};
+      cams_info_.back().R = {1.0, 0.0, 0.0,
+                       0.0, 1.0, 0.0,
+                       0.0, 0.0, 1.0};
+      cams_info_.back().P = {camera.f, 0.0, camera.cx, 0.0,
+                       0.0, camera.f, camera.cy, 0.0,
+                       0.0, 0.0, 1, 0.0};
+
+      // FIXME For stereo camera we account for baseline in field "P"
+      /*
+      double baseline = (visim_project_.T_SC_left.getPosition() -
                          visim_project_.T_SC_right.getPosition()).norm();
 
       cam_info_r_.P = {visim_project_.f, 0.0, visim_project_.cx, -visim_project_.f * baseline,
                        0.0, visim_project_.f, visim_project_.cy, 0.0,
                        0.0, 0.0, 1, 0.0};
+      */
+
+      // Camera publisher
+      cams_pub_.push_back(image_transport::CameraPublisher());
+      cams_pub_.back() = image_transport_.advertiseCamera("cam_" + std::to_string(i) + "/image", 1);
+
+      // Images publishers
+      depth_pub_.push_back(image_transport_.advertise("depth_map_" + std::to_string(i), 1));
+      semantic_pub_.push_back(image_transport_.advertise("semantic_map_" + std::to_string(i), 1));
+      cameras_odom_pub_.push_back(nh_private_.advertise<nav_msgs::Odometry>("camera_odometry_out_" + std::to_string(i), 50));
+
+      // Output from renderer
+      result_depth_map_.push_back(cv::Mat());
+      result_rgbs_map_.push_back(cv::Mat());
+      result_rgb_map_.push_back(cv::Mat());
+      result_s_map_.push_back(cv::Mat());
+
+      result_rgb_map_.back().create(camera.h, camera.w, CV_8UC3);
+      result_s_map_.back().create(camera.h, camera.w, CV_8UC1);
+
+      // Add TF broadcasters
+      tf_broadcasters_.push_back(tf::TransformBroadcaster());
+
+      // Calculate tf
+      tf::StampedTransform transform;
+      transform.setOrigin(tf::Vector3(p_x, p_y, p_z));
+
+      tf::Quaternion q_tf;
+      Eigen::Quaterniond q_rpy(T_SC.block<3,3>(0,0));
+      q_tf.setX(q_rpy.x());
+      q_tf.setY(q_rpy.y());
+      q_tf.setZ(q_rpy.z());
+      q_tf.setW(q_rpy.w());
+      transform.setRotation(q_tf);
+
+      transform.frame_id_ = body_frame;
+      transform.child_frame_id_ = cams_info_.back().header.frame_id;
+
+      tf_cameras_.push_back(transform);
     }
 
-    //dense_pointcloud_pub_ =
-    //        nh_private_.advertise<sensor_msgs::PointCloud>("labelled_dense_pointcloud", 5); //TODO add param to enable the point cloud publication
-
-    result_rgb_map_l_.create(visim_project_.h,visim_project_.w,CV_8UC3);
-    result_s_map_l_.create(visim_project_.h,visim_project_.w,CV_8UC1);
-    result_rgb_map_r_.create(visim_project_.h,visim_project_.w,CV_8UC3);
-    result_s_map_r_.create(visim_project_.h,visim_project_.w,CV_8UC1);
 
     // Initialization is fine
     initialized_ = true;
@@ -122,12 +167,13 @@ void VRGlassesNode::run()
     
     nh_private_.param("render_near",near_,near_);    
 
-    renderer_ = new vrglasses_for_robots::VulkanRenderer(visim_project_.w, 
-          visim_project_.h, near_, far_, shader_folder);
+    auto camera = visim_project_.camera;
+    renderer_ = new vrglasses_for_robots::VulkanRenderer(camera.w,
+          camera.h, near_, far_, shader_folder);
     glm::mat4 empty;
     buildOpenglProjectionFromIntrinsics(perpective_, empty, 
-            visim_project_.w, visim_project_.h, visim_project_.f,
-            visim_project_.f, 0, visim_project_.cx, visim_project_.cy, near_, far_);
+            camera.w, camera.h, camera.f,
+            camera.f, 0, camera.cx, camera.cy, near_, far_);
 
     std::string mesh_obj_file;
     std::string texture_file;
@@ -161,8 +207,6 @@ void VRGlassesNode::run()
         ROS_ERROR("mesh_obj_file and texture_file need to be defined parameter, alternatively model_folder and model_list_file");
     }
 
-
-    
     
     while (::ros::ok()) {
       ::ros::spinOnce();
@@ -181,106 +225,53 @@ void VRGlassesNode::odomCallback(const nav_msgs::Odometry &msg)
     if( msg.header.stamp - last_frame_time_ >= diff_frames_)
     {
         last_frame_time_ = msg.header.stamp;
-        kindr::minimal::QuatTransformation T_WC_left = computeT_WC_left(msg.pose.pose);
-        glm::mat4 mvp_left = computeMVP(T_WC_left);
-        renderer_->setCamera(mvp_left);
-        renderer_->renderMesh(result_depth_map_l_, result_rgbs_map_l_);
 
-        nav_msgs::Odometry odom_msg = msg;        
-        tf::poseKindrToMsg(T_WC_left,&(odom_msg.pose.pose));
-        camera_odom_pub_.publish(odom_msg);
+        // Iterate over the cameras
+        for (size_t i = 0; i < result_s_map_.size(); ++i) {
+          // Fetch images from renderer
+          kindr::minimal::QuatTransformation T_WC_left = computeT_WC_camera(i, msg.pose.pose);
+          glm::mat4 mvp_left = computeMVP(T_WC_left);
+          renderer_->setCamera(mvp_left);
+          renderer_->renderMesh(result_depth_map_[i], result_rgbs_map_[i]);
 
-        cv::Mat out_l[] = { result_rgb_map_l_,result_s_map_l_ };
-        int from_to[] = { 0,2, 1,1, 2,0, 3,3 };
-        cv::mixChannels(&result_rgbs_map_l_,1,out_l,2,from_to,4);
-        sensor_msgs::ImagePtr rgb_msg_l, rgb_msg_r;
-        rgb_msg_l = cv_bridge::CvImage(msg.header, "rgb8", result_rgb_map_l_).toImageMsg();
-        rgb_msg_l->header.frame_id = cam_info_l_.header.frame_id;
-        
-        cam_info_l_.header.seq = msg.header.seq;
-        cam_info_l_.header.stamp = msg.header.stamp;
+          // RGB
+          cv::Mat out_l[] = { result_rgb_map_[i], result_s_map_[i] };
+          int from_to[] = { 0,2, 1,1, 2,0, 3,3 };
+          cv::mixChannels(&result_rgbs_map_[i],1,out_l,2,from_to,4);
+          sensor_msgs::ImagePtr rgb_msg;
+          rgb_msg = cv_bridge::CvImage(msg.header, "rgb8", result_rgb_map_[i]).toImageMsg();
+          rgb_msg->header.frame_id = cams_info_[i].header.frame_id;
 
-        cam_l_pub_.publish(*rgb_msg_l, cam_info_l_);
+          cams_info_[i].header.seq = msg.header.seq;
+          cams_info_[i].header.stamp = msg.header.stamp;
 
-        if(stereo_){
-          kindr::minimal::QuatTransformation T_WC_right = computeT_WC_right(msg.pose.pose);
-          glm::mat4 mvp_right = computeMVP(T_WC_right);
-          renderer_->setCamera(mvp_right);
-          renderer_->renderMesh(result_depth_map_r_, result_rgbs_map_r_);
-          
-          cv::Mat out_r[] = { result_rgb_map_r_,result_s_map_r_ };
-          cv::mixChannels(&result_rgbs_map_r_,1,out_r,2,from_to,4);
-          rgb_msg_r = cv_bridge::CvImage(msg.header, "rgb8", result_rgb_map_r_).toImageMsg();
-          rgb_msg_r->header.frame_id = cam_info_r_.header.frame_id;
+          cams_pub_[i].publish(*rgb_msg, cams_info_[i]);
 
-          cam_info_r_.header.seq = msg.header.seq;
-          cam_info_r_.header.stamp = msg.header.stamp;
+          // Semantic
+          sensor_msgs::ImagePtr semantic_msg;
+          semantic_msg = cv_bridge::CvImage(msg.header, "mono8", result_s_map_[i]).toImageMsg();
+          semantic_msg->header.frame_id = cams_info_[i].header.frame_id;
+          semantic_pub_[i].publish(semantic_msg);
 
-          cam_r_pub_.publish(*rgb_msg_r, cam_info_r_);
+          // Depth
+          sensor_msgs::ImagePtr depth_msg;
+          depth_msg = cv_bridge::CvImage(msg.header, "32FC1", result_depth_map_[i]).toImageMsg();
+          depth_msg->header.frame_id = cams_info_[i].header.frame_id;
+          depth_pub_[i].publish(depth_msg);
+
+          // Camera odometry
+          nav_msgs::Odometry odom_msg = msg;
+          tf::poseKindrToMsg(T_WC_left,&(odom_msg.pose.pose));
+          cameras_odom_pub_[i].publish(odom_msg);
+
+          // Publish TF
+          tf_cameras_[i].stamp_ = ros::Time::now();
+          tf_broadcasters_[i].sendTransform(tf_cameras_[i]);
         }
-
-        sensor_msgs::ImagePtr semantic_msg;
-        semantic_msg = cv_bridge::CvImage(msg.header, "mono8", result_s_map_l_).toImageMsg();
-        semantic_msg->header.frame_id = cam_info_l_.header.frame_id;
-        semantic_pub_.publish(semantic_msg);
-
-        sensor_msgs::ImagePtr depth_msg;
-        depth_msg = cv_bridge::CvImage(msg.header, "32FC1", result_depth_map_l_).toImageMsg();
-        depth_msg->header.frame_id = cam_info_l_.header.frame_id;
-        depth_pub_.publish(depth_msg);
-
-        //publishDenseSemanticCloud(msg.header,depth_msg,result_s_map_); //TODO add param to enable the point cloud publication
     }
 }
 
-void VRGlassesNode::publishDenseSemanticCloud(const ::std_msgs::Header header, const sensor_msgs::ImagePtr &depth_map, const cv::Mat &semantic_map)
-{
-    cv_bridge::CvImageConstPtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvShare(depth_map);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    sensor_msgs::PointCloud labelled_pointcloud;
-    labelled_pointcloud.header = header;
-
-    sensor_msgs::ChannelFloat32 quality_channel;
-    quality_channel.name = "quality";
-
-    float* data = (float*)cv_ptr->image.data;
-    for (int u = 0; u < cv_ptr->image.cols; u += 4)
-    {
-      for (int v = 0; v < cv_ptr->image.rows; v += 4)
-      {
-        float val = data[v * cv_ptr->image.cols + u];
-        float semantic_val = semantic_map.at<uchar>(v,u)/255.0;
-        if (std::isfinite(val))
-        {
-
-          geometry_msgs::Point32 p_C_msg;
-          p_C_msg.x = val * (u - visim_project_.cx) / visim_project_.f;
-          p_C_msg.y = val * (v - visim_project_.cy) / visim_project_.f;
-          p_C_msg.z = val;
-
-          quality_channel.values.push_back(semantic_val);
-          labelled_pointcloud.points.push_back(p_C_msg);
-        }
-      }
-    }
-
-    // Now add the channel indicating the quality
-    labelled_pointcloud.channels.push_back(quality_channel);
-
-    // Publish the pointcloud
-    dense_pointcloud_pub_.publish(labelled_pointcloud);
-  }
-
-kindr::minimal::QuatTransformation VRGlassesNode::computeT_WC_left(const geometry_msgs::Pose &pose)
+kindr::minimal::QuatTransformation VRGlassesNode::computeT_WC_camera(size_t cam_id, const geometry_msgs::Pose &pose) const
 {
     Eigen::Vector3d p_WS(pose.position.x, pose.position.y, pose.position.z);
 
@@ -292,22 +283,7 @@ kindr::minimal::QuatTransformation VRGlassesNode::computeT_WC_left(const geometr
     q_WS.normalize();
 
     kindr::minimal::QuatTransformation T_WC =
-            kindr::minimal::QuatTransformation(p_WS, q_WS) * visim_project_.T_SC_left;
-
-    return T_WC;
-}
-kindr::minimal::QuatTransformation VRGlassesNode::computeT_WC_right(const geometry_msgs::Pose &pose)
-{
-    Eigen::Vector3d p_WS(pose.position.x, pose.position.y, pose.position.z);
-
-    Eigen::Quaterniond q_WS;
-    q_WS.x() = pose.orientation.x;
-    q_WS.y() = pose.orientation.y;
-    q_WS.z() = pose.orientation.z;
-    q_WS.w() = pose.orientation.w;
-
-    kindr::minimal::QuatTransformation T_WC =
-            kindr::minimal::QuatTransformation(p_WS, q_WS) * visim_project_.T_SC_right;
+            kindr::minimal::QuatTransformation(p_WS, q_WS) * visim_project_.T_SCs[cam_id];
 
     return T_WC;
 }
